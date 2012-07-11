3d0dc69db050879c065a9393d3c64b41fc808e3f
spartango@gmail.com <spartango@gmail.com>
2012-07-11 16:54:28 -0400

    got PCA to not break

--- src/edu/harvard/mcb/leschziner/classify/PCAClassifier.java
+++ src/edu/harvard/mcb/leschziner/classify/PCAClassifier.java
@@ -35,36 +35,46 @@ public class PCAClassifier extends DistributedParticleConsumer {
     public void classifyAll() {
         if (targets.size() > 0) {
             int particleSize = targets.get(0).getSize();
-            CvMat targetMat = CvMat.create(particleSize * particleSize,
-                                           targets.size(), opencv_core.CV_32FC1);
+            int particleArea = particleSize * particleSize;
+            // Each row is an image, each column is a pixel
+            CvMat targetMat = CvMat.create(targets.size(), particleArea,
+                                           opencv_core.CV_32FC1);
             // Reshape the targets for use in the matrix
             for (int i = 0; i < targets.size(); i++) {
                 IplImage targetImage = targets.get(i).getImage();
-                System.out.println("Channels: " + targetImage.nChannels());
-                CvMat row = CvMat.create(1, particleSize * particleSize,
-                                         targetImage.depth(),
-                                         targetImage.nChannels());
-                opencv_core.cvReshape(targetImage, row, 0, 0);
-                // Copy into the target matrix
-                CvMat targetRow = CvMat.createHeader(1, particleSize
-                                                        * particleSize,
+                // A single row containing all the pixels in the image
+                CvMat row = CvMat.createHeader(1, particleArea,
+                                               targetImage.depth(),
+                                               targetImage.nChannels());
+                opencv_core.cvReshape(targetImage, row,
+                                      targetImage.nChannels(), row.rows());
+
+                // A 32 bit float row from the pca matrix
+                CvMat targetRow = CvMat.createHeader(1, particleArea,
                                                      opencv_core.CV_32FC1);
                 opencv_core.cvGetRow(targetMat, targetRow, i);
+
+                // Convert the image data row to 32 bit float, normalizing it to
+                // 0-1 scale
                 opencv_core.cvConvertScale(row, targetRow, 1 / 255.0, 0);
             }
 
             // Run PCA on the target matrix
+
             CvMat eigenValues = CvMat.create(principalComponentCount, 1);
             CvMat eigenVectors = CvMat.create(principalComponentCount,
-                                              particleSize * particleSize,
+                                              particleArea,
                                               opencv_core.CV_32FC1);
-            opencv_core.cvCalcPCA(targetMat, new CvMat(null), eigenValues,
-                                  eigenVectors, opencv_core.CV_PCA_DATA_AS_ROW);
+            CvMat averages = CvMat.create(1, particleArea, opencv_core.CV_32FC1);
+
+            opencv_core.cvCalcPCA(targetMat, averages, eigenValues,
+                                  eigenVectors, opencv_core.CV_PCA_DATA_AS_ROW
+                                                | opencv_core.CV_PCA_USE_AVG);
 
             CvMat subspace = CvMat.create(targets.size(),
                                           principalComponentCount);
             // Project onto a subspace
-            opencv_core.cvProjectPCA(targetMat, new CvMat(null), eigenVectors,
+            opencv_core.cvProjectPCA(targetMat, averages, eigenVectors,
                                      subspace);
 
             CvTermCriteria terminationCriteria = new CvTermCriteria(
